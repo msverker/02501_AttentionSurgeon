c98ad432c400c804509a84b5707cc38caddb2806
kasper1616 <s214786@gmail.com>
2026-04-26 19:18:36 +0200

    change to a100

--- jobs/train_cls.sh
+++ jobs/train_cls.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #BSUB -J train_probe
-#BSUB -q gpuv100
+#BSUB -q gpua100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -n 4
 #BSUB -R "rusage[mem=8GB]"
 #BSUB -W 4:00
-#BSUB -o logs/train_%J.out
-#BSUB -e logs/train_%J.err
 
 uv run src/train.py --task cls --epochs 5
